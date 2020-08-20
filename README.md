5ad4035117a99ce451f048e1a2dc9d003dcb3139
Ricardo <ricardocavalcantesilva@gmail.com>
2020-08-19 23:41:21 -0300

    Commit do front end

---
+++ README.md
@@ -0,0 +1,15 @@
+Anotações
+
+Babel: Converter (transpilar) código do React para um código que o browser entenda
+
+Webpack: Pra cada arquivo (.js, .css, .png) eu vou converter o código de uma maneira que o browser entenda
+ 
+Loaders: babel-loader, css-loader, image-loader
+
+JSX: HTML dentro do Javascript (Javascript XML)
+
+Na função App.js foi utilizado o modelo fragment (<> e </>) que serve como um container nos objetos
+
+useState retorna um array com duas posições
+1. Variável com seu valor inicial
+2. Função para atualizarmos esse valor
---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+
+import './App.css';
+import backgroundImage from './assets/background.jpg';
+
+import Header from './components/Header';
+
+function App() {
+    const [projects, setProjects] = useState(['Desenvolvimento de app', 'Front-end web']);
+
+    function handleAddProject() {
+        setProjects([...projects,`Novo projeto ${Date.now()}`]);
+    }
+    return (
+        <>
+            <Header title="Projects" />
+
+            <img src={backgroundImage} width={300} />
+
+            <ul>
+                {projects.map(project => <li key={project}>{project}</li>)}
+            </ul>
+
+            <button type="button" onClick={handleAddProject}>Adicionar Projeto</button>
+        </>
+    );
+}
+
+export default App;
